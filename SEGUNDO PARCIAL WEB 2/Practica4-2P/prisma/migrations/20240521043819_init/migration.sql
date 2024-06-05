183c13a4b002c40a40b43e3253f97fe1db1632f9
mosquera23-uleam <167156572+mosquera23-uleam@users.noreply.github.com>
2024-06-04 20:42:03 -0500

    Delete SEGUNDO PARCIAL WEB 2/Practica4-2P directory

--- SEGUNDO PARCIAL WEB 2/Practica4-2P/prisma/migrations/20240521043819_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `idMesero` on the `preparacion` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "preparacion" DROP COLUMN "idMesero",
-ALTER COLUMN "tiempoEstimado" SET DATA TYPE TEXT;
